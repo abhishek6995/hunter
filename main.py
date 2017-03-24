b96afa551afd9d61b66cab7f205aaef4df4c1629
abhishek <kumarabhishek786.akv@gmail.com>
2017-03-24 11:16:10 +0530

    all projects are done

---
+++ main.py
@@ -0,0 +1,9 @@
+choice=input("choose between 1/2/3")
+if choice==1:
+	import student_become_teacher.py
+elif choice==2:
+	import battleship.py
+elif choice==3:
+	import exam statistcs.py
+else:
+	print "enter your choice again !"
